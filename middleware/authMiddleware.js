e4bb2f76e4254f40c3d4c71c6cd1bc7b96a7ba23
harshitazzz <harshitamaheshwari321@gmail.com>
2025-09-29 22:22:57 +0530

    maybe last commit hope so

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,12 @@
 const verifySecret=(req,res,next)=>{
-    const incomingShippingSecret=req.headers["shipping_secret_key"];
+    const incomingShippingSecret=req.headers["SHIPPING_SECRET_KEY"];
     const localSecretKey= process.env.SHIPPING_SECRET_KEY
     if(!incomingShippingSecret){
         return res.status(403).json({error:"SHIPPING_SECRET_KEY is missing or invalid."});
     }
     if(incomingShippingSecret !== localSecretKey){
         return res.status(403).json({
-            error:"Failed to authenticate SHIPPING_SECRET_KEY",
+            error:"Failed to authenticate SHIPPING_SECRET_KEY"
         });
     }
     next()
