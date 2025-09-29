63b40f4486ef0b5f392008906ad7f5a1e510d333
harshitazzz <harshitamaheshwari321@gmail.com>
2025-09-29 16:52:04 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const { verifySecret } = require('./middleware/authMiddleware');
+const prisma = new PrismaClient();
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(verifySecret)
+
+app.post("/api/shipping/create",async(req,res)=>{
+  try{
+    const {userId,productId,count}=req.body;
+    if(!userId || !productId || !count){
+      return res.status(404).json({"error": "All fields required"})
+    }
+    const shipping= await prisma.shipping.create({
+      data:{userId,productId,count}
+    });
+    return res.status(200).json(shipping);
+  }
+  catch(error){
+    console.log(error);
+    return res.status(500).json({err:"Internal Sevrer error"})
+  }
+});
+
+app.put("/api/shipping/cancel",async(req,res)=>{
+  try{
+    const {shippingId}=req.body;
+    if(!userId || !productId || !count){
+      return res.status(404).json({"error": "Missing shippingId", })
+    }
+    const shipping= await prisma.shipping.update({
+      where:{id:shippingId},
+      data:{status:"cancelled"},
+    });
+    return res.status(200).json(shipping);
+  }
+  catch(error){
+    console.log(error);
+    return res.status(500).json({err:"Internal Sevrer error"})
+  }
+});
+
+
+app.get("/api/shipping/get",async(req,res)=>{
+  try{
+    const {userId}=req.query;
+    if(!userId){
+      const shipping= await prisma.shipping.findMany();
+      return res.status(200).json(shipping);
+    }
+    const shipping = await prisma.shipping.findMany({
+      where:{id:userId},
+    })
+    return res.status(200).json(shipping)
+  }
+  catch(error){
+    console.log(error);
+    return res.status(500).json({err:"Internal Sevrer error"})
+  }
+});
+
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
